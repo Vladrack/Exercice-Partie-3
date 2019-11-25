cc9c172ae0f60ed6e1541a2b9faf18044f65d1c9
Vladrack <frg.cedric@gmail.com>
2019-11-25 14:38:58 +0100

    exercicepartie3CSSJS

---
+++ assets/JS/exo3partiehtmlETJS.js
@@ -0,0 +1,30 @@
+window.addEventListener('scroll', function () {
+    var scrollValue = window.scrollY;
+    if (scrollValue >= 250) {
+        document.getElementById('floatGeneral').style.opacity = 1;
+        document.getElementById('floatGeneral').style.transition = 'opacity 2s';
+    } else {
+        document.getElementById('floatGeneral').style.opacity = 0;
+    }
+
+})
+window.addEventListener('scroll', function () {
+    var scrollValue = window.scrollY;
+    if (scrollValue >= 250) {
+        document.getElementById('floatGeneral').style.opacity = 1;
+        document.getElementById('floatGeneral').style.transition = 'opacity 2s';
+    } else {
+        document.getElementById('floatGeneral').style.opacity = 0;
+    }
+
+})
+window.addEventListener('scroll', function () {
+    var scrollValue = window.scrollY;
+    if (scrollValue >= 250) {
+        document.getElementById('floatGeneral').style.opacity = 1;
+        document.getElementById('floatGeneral').style.transition = 'opacity 2s';
+    } else {
+        document.getElementById('floatGeneral').style.opacity = 0;
+    }
+
+})
